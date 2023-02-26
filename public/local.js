7db2d6ee5b93f3644afcfff72b047723a5ae998d
tomdejong2000 <tomdejong2000@gmail.com>
2023-02-26 13:27:34 +0100

    Update local.js

--- public/local.js
+++ public/local.js
@@ -1,28 +1,28 @@
 console.log("kaas");
 
+$(document).ready(function(){
+  $(".hamburger-lines").click(function() {
+      $(".line1").toggleClass("animated1");
+      $(".line2").toggleClass("animated2");
+      $(".line3").toggleClass("animated3");
 
-$(".hamburger-lines").click(function() {
-    $(".line1").toggleClass("animated1");
-    $(".line2").toggleClass("animated2");
-    $(".line3").toggleClass("animated3");
+      $(".nav").toggleClass("opened");
+      $("body").toggleClass("stopscroll")
 
-    $(".nav").toggleClass("opened");
-    $("body").toggleClass("stopscroll")
-
-  });
-
-
-//fetch de data uit json object uit /data en stop dat in h2
-let fetchdata = function(){
-  fetch('/data').then(response => response.json()).then (jsonD => {
-
-      $(".stat1").text(jsonD["message1"])
-      $(".stat2").text(jsonD["message2"])
-      $(".stat3").text(jsonD["message3"])
-  
-      
     });
-}
 
-setInterval(function() {fetchdata();},1000)
 
+  //fetch de data uit json object uit /data en stop dat in h2
+  let fetchdata = function(){
+    fetch('/data').then(response => response.json()).then (jsonD => {
+
+        $(".stat1").text(jsonD["message1"])
+        $(".stat2").text(jsonD["message2"])
+        $(".stat3").text(jsonD["message3"])
+    
+        
+      });
+  }
+
+  setInterval(function() {fetchdata();},1000)
+});
